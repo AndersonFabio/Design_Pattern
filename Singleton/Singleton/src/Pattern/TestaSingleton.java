83e3ccdea8f2b36b32eec0156b93bb943d8327ae
Anderson Fábio <andersonfabio.1976@gmail.com>
2018-04-07 18:36:23 -0300

    Update TestaSingleton.java

--- Singleton/Singleton/src/Pattern/TestaSingleton.java
+++ Singleton/Singleton/src/Pattern/TestaSingleton.java
@@ -3,13 +3,11 @@ package Pattern;
 public class TestaSingleton {
 
 	public static void main(String[] args) {
-		Configuracao configuracao = Configuracao.getInstance();
-		System.out.println(configuracao.getPropriedade("time-zone"));
-		System.out.println(configuracao.getPropriedade("currency-code"));
+		System.out.println(Configuracao.getInstance().getPropriedade("time-zone"));
+		System.out.println(Configuracao.getInstance().getPropriedade("currency-code"));
 		
-		Configuracao configuracao2 = Configuracao.getInstance();
-		System.out.println(configuracao2.getPropriedade("time-zone"));
-		System.out.println(configuracao2.getPropriedade("currency-code"));
+		System.out.println(Configuracao.getInstance().getPropriedade("time-zone"));
+		System.out.println(Configuracao.getInstance().getPropriedade("currency-code"));
 		
 	}
 
